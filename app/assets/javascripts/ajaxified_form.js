4d6788dea252ad63040629a1d0f081d922b8c9cf
Sherry Seibel <sherry.seibel@gmail.com>
2014-07-09 16:58:49 -0400

    Add links to task view

--- app/assets/javascripts/ajaxified_form.js
+++ app/assets/javascripts/ajaxified_form.js
@@ -3,27 +3,36 @@ $(function(){
     $.post(
       "/tasks",
       $("#new_task_form").serialize()
-    ).done(function(html){
-      $("#tasks").prepend(html);
-      $("#errors").html("");
-      $("#task_title, #task_body").val("");
-    }).fail(function(xhr){
-      var html = xhr.responseText;
-      $("#errors").html(html);
-    });
+      ).done(function(html){
+        $("#tasks").prepend(html);
+        $("#errors").html("");
+        $("#task_title, #task_body").val("");
+      }).fail(function(xhr){
+        var html = xhr.responseText;
+        $("#errors").html(html);
+      });
 
     return false;
   })
 
-  $("body").on("click", ".completed_checkbox", function(event){
-      var formEl = $(event.target).parents("form");
-      $.ajax({
-        url: formEl.prop("action"),
-        data: formEl.serialize(),
-        type: "PATCH"
-      }).done(function(){
-       formEl.parents("li").fadeOut();
-     });
+  $("#tasks").on("click", ".completed_checkbox", function(event){
+    var formEl = $(event.target).parents("form");
+    $.ajax({
+      url: formEl.prop("action"),
+      data: formEl.serialize(),
+      type: "PATCH"
+    }).done(function(){
+      formEl.parents("li").fadeOut();
+    });
+  });
+
+  $("#tasks").on("click", "#delete", function(event){
+    var spanEl = $(event.target).parents("form");
+    $("span").click(function(){
+      $("#delete").toggle();
+    });
+
+    return false;
   });
 });
 
